e91f5b1826d0cdf87c0783f1fccaff0b71e445a1
Xiangru Guo <guoxiangru129@gmail.com>
2014-11-26 10:23:25 +0800

    Fixes the problem that the boundary value of the decryped string

--- swift/encryption/utils/encryptionutils.py
+++ swift/encryption/utils/encryptionutils.py
@@ -12,11 +12,10 @@ def encrypt(key, in_str):
     out_str = ""
     iv = ''.join(chr(random.randint(0, 0xFF)) for i in range(16))
     encryptor = AES.new(key, AES.MODE_CBC, iv)
-    if len(in_str) % 16 != 0:
-         last_space = 16 - len(in_str) % 16
-         if last_space < 10:
-            last_space = str(last_space).rjust(2, '0')
-    out_str = out_str + str(last_space) + iv
+    extra_space = 16 - len(in_str) % 16
+    if extra_space < 10:
+        extra_space = str(extra_space).rjust(2, '0')
+    out_str = out_str + str(extra_space) + iv
     for chunk in chunks(in_str, chunk_size, 0):
         if len(chunk) == 0:
             break
@@ -25,21 +24,20 @@ def encrypt(key, in_str):
         out_str = out_str + encryptor.encrypt(chunk)
     return out_str
 
-
 def decrypt(key, in_str):
-    chunk_size=24 * 1024
+    chunk_size = 24 * 1024
     out_str = ""
-    last_space = int(in_str[0:2])
+    extra_space = int(in_str[0:2]) % 16
     iv = in_str[2:18]
     decryptor = AES.new(key, AES.MODE_CBC, iv)
     for chunk in chunks(in_str, chunk_size, 18):
         if len(chunk) == 0:
                 break
         out_str = out_str + decryptor.decrypt(chunk)
-        out_str = out_str[0:len(out_str)-last_space]
+        out_str = out_str[0:len(out_str)-extra_space]
     return out_str
 
 
 def chunks(s, n, st):
-    for start in range(st,len(s),n):
+    for start in range(st, len(s), n):
         yield s[start:start+n]
